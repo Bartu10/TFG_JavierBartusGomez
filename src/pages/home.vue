77d250c3e9c5e014de2eb74da90a0246d05fbeca
Bartu10 <jbg10102002@gmail.com>
2023-04-14 13:09:40 +0200

    clase

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -11,30 +11,64 @@
         </span>
     </div>
         <div class="third">
+            <div class="Question">
         <span>
-            third
+            ¿Quiénes somos?
         </span>
+            </div>
         </div>
     </div>
 </template>
 
 
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../scss/global.scss';
+
+
+
 .first{grid-area: first;
+background-color: rgba(255, 255, 255, 0.7);
+margin-top:20rem;
+margin-left:15rem;
+margin-right: 15rem;
+
+}
+.second{
+grid-area: second;
 background-color: rgba(255, 255, 255, 0.5);
 margin-top:20rem;
 margin-left:15rem;
 margin-right: 15rem;
-}
-.second{grid-area: second;
-    background-color: rgba(255, 255, 255, 0.5);
-margin-top:20rem;
-margin-left:15rem;
-margin-right: 15rem;}
+align-items: center;
+justify-content: center;
+display: flex;
 
-.third{grid-area: third;
-    background-color: grey;}
+
+span{
+color: green;
+}
+}
+
+
+.third{
+font-family: 'WorkSans';
+font-size:4em ;
+grid-area: third;
+margin-top:10%;
+margin-bottom: 10%;
+background-color: $principalGreen;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+
+.Question{
+text-align: center;
+justify-content: center;    
+
+}
+}
 .main{
     display: grid;
     grid-template: 
